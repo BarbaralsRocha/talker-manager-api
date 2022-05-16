b9e04d4c01f2ed069a3b20819d55eb32635aacb8
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-05-16 17:46:24 -0300

    change: update middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,21 @@
 const errorHandler = require('./errorHandler');
 const emailValidation = require('./emailValidation');
 const passValidation = require('./passValidation');
+const tokenValidation = require('./tokenValidation');
+const nameValidation = require('./nameValidation');
+const ageValidation = require('./ageValidation');
+const talkValidation = require('./talkValidation');
+const watchetAtValidation = require('./watchetAtValidation');
+const rateValidation = require('./rateValidation');
 
 module.exports = { 
     errorHandler,
     emailValidation,
     passValidation,
+    tokenValidation,
+    nameValidation,
+    ageValidation,
+    talkValidation,
+    watchetAtValidation,
+    rateValidation,
  };
