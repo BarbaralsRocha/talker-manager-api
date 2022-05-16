9cd7d7b04bcc1d359c8210338b52b7cb5361f523
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-05-16 17:46:05 -0300

    fix: clear console

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,5 @@
-const errorHandler = (error, _req, res, _next) => {
-    console.log('erro');
-    return res.status(error.status || 500).json({ message: error.message });
-};
+const errorHandler = (error, _req, res, _next) => (
+    res.status(error.status || 500).json({ message: error.message })
+);
 
 module.exports = errorHandler;
