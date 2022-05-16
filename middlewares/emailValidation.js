469573e2a2edac0139f52fe958851f368d9697e8
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-05-16 17:45:48 -0300

    fix: return resolve

--- middlewares/emailValidation.js
+++ middlewares/emailValidation.js
@@ -1,13 +1,12 @@
 const emailValidation = (req, res, next) => {
-    console.log('loginValidation');
     const { email } = req.body;
     if (!email || email === '') {
-        res.status(400).json({ message: 'O campo "email" é obrigatório' });
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
     }
     const regex = /\S+@\S+\.\S+/;
     const validateEmail = regex.test(email);
     if (validateEmail === false) {
-        res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
     }
     next();
 };
